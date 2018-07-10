a115f6af682feafe3a5320900111b379e92a26ec
blunkka <aki@Akis-MacBook-Pro.local>
2018-07-10 16:07:19 +0300

    updates

--- js/main-jquery_i18n.js
+++ js/main-jquery_i18n.js
@@ -29,7 +29,20 @@ jQuery(document).ready(function() {
       'nav-contact': 'Contact me',
       'nav-close': 'Close x',
       'title-intro': 'Party photoshoot, family photoshoot, portrait photoshoot, pet photoshoot',
-      'title-portfolio': 'Portfolio'
+      'text-intro': '',
+      'header-intro': '',
+      'title-portfolio': 'Portfolio',
+      'header-about': '',
+      'text-about-p1': '',
+      'text-about-p2': '',
+      'text-about-p3': '',
+      'text-about-p4': '',
+      'text-about-p4': '',
+      'text-about-p5': '',
+      'text-about-p6': '',
+      'text-about-p7': '',
+      'text-about-p8': '',
+      'text-about-p9': ''
     },
     'fi': {
       'nav-home': 'Etusivu',
@@ -38,7 +51,20 @@ jQuery(document).ready(function() {
       'nav-contact': 'Ota yhteyttä',
       'nav-close': 'Sulje x',
       'title-intro': 'Juhlakuvaus, perhekuvaus, potrettikuvaus, lemmikkikuvaus',
-      'title-portfolio': 'Portfolio'
+      'text-intro': 'Minä olen Julia Blomqvist, 31-vuotias perhevalokuvaaja Helsingistä. Uskon vakaasti siihen, että jokainen yksilö, ihmissuhde ja hetki on omalla tavallaan ainutlaatuinen ja kaunis. Koen onnistuneeni valokuvaajana parhaiten silloin, kun pystyn ikuistamaan tämän kauneuden niin, että lopputulos on mahdollisimman luonnollinen. En siis pyri valokuvauksessa mainosmaisiin otoksiin, vaan tavoitteenani on ottaa kuvia, joista sinun on helppo tunnistaa omasi ja läheistesi parhaat puolet. Kuvia, jotka auttavat sinua muistamaan niitä hetkiä, ihmissuhteita ja asioita, jotka ovat elämässäsi tärkeitä. Minut voi tilata valokuvaamaan ympäri Suomea, vaikkakin kuvaan pääsääntöisesti pääkaupunkiseudulla. Ota rohkeasti yhteyttä ja suunnitellaan yhdessä ikimuistoinen kuvauspäivä!',
+      'header-intro': 'Kiva, kun olet täällä!',
+      'title-portfolio': 'Portfolio',
+      'header-about': 'Moikka vaan!',
+      'text-about-p1': 'Olen kolmekymppinen valokuvaaja Helsingistä, joka on aina inspiroitunut Ihmisistä ja heidän tarinoistaan.',
+      'text-about-p2': 'Valokuvaus on ollut iso osa elämääni jo pienestä pitäen, sillä isäni harrasti intohimoisesti valokuvausta jo ollessani pieni. Hänen tekemänsä kuva-albumit ovat auttaneet minua muistamaan asioita lapsuudestani ja monesta kuvasta on tullut minulle erittäin tärkeä.',
+      'text-about-p3': 'Innostuin itse olemaan kameran takana saatuani lahjaksi ensimmäisen  järjestelmäkamerani vuonna 2012. Ryhdyin oitis kuvaamaan arjessa ja ulkomaanmatkoilla, mutten olisi tuolloin vielä uskonut valokuvauksesta tulevan minulle muuta kuin harrastus.',
+      'text-about-p4': 'Kesällä 2017 elämäni oli eräänlaisessa taitekohdassa. Valmistuin yliopistosta, mutta koin, etten ollut valmis siirtymään kaksin jaloin työelämään. Halusin tehdä työkseni jotain luovempaa, kuin mitä omalta alaltani valmistuneet tyypillisesti tekevät. Urahaaveissani olikin eräänlainen musta aukko.',
+      'text-about-p4': 'Syksyllä 2017 aloitin medianomin opinnot Helsingin avoimessa ammattikorkeakoulussa, jossa olen opiskellut muun muassa valokuvauksellista ilmaisua.',
+      'text-about-p5': 'Pyrin valokuvauksessa luonnollisuuteen. Tyylissäni voi havaita rosoisuutta ja historian havinaa. Rakkauteni luontoon ja eläimiin on myös jättänyt jälkensä kuvakerrontaani.',
+      'text-about-p6': 'Palvelen asiakkaitani suomen kielen lisäksi tarvittaessa myös englanniksi tai saksaksi.',
+      'text-about-p7': 'Jos kiinnostuksesi heräsi, minuun saa parhaiten yhteyttä tämän sivuston yhteydenottolomakkeen avulla. Pyrin vastaamaan 24 tunnin sisällä.',
+      'text-about-p8': 'Suhtaudun avoimin mielin erilaisiin projekteihin ja teen parhaani, jotta valokuvat tuottaisivat sinulle hyvää mieltä läpi elämän.',
+      'text-about-p9': 'Yhteydenottoasi odottaen'
     }
   });
 
